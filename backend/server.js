110c138be50fdad7963a039d8c7a4b9ccb407e62
Priyansh Kasera <priyanshkasera.ppkk@gmail.com>
2024-02-09 00:55:45 +0530

    env variable path set

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
-const app = require('./app');
-const dotenv = require('dotenv')
-console.log("one two")
-const connectDatabase = require("./config/databaseConnection")
+const app = require("./app");
+const dotenv = require("dotenv");
+const path = require("path");
+console.log("one two");
+const connectDatabase = require("./config/databaseConnection");
 //config
-dotenv.config({path : "backend/config/config.env"})
-console.log("logging from here")
-connectDatabase()
+dotenv.config({ path: `${__dirname}/config/config.env` });
+console.log("logging from here");
+connectDatabase();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`sever running on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`sever running on port ${process.env.PORT}`);
+});
