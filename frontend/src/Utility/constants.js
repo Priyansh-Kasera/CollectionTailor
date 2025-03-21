c18c6958d166edb6cad5a33b525a33b23c21087a
Priyansh Kasera <priyanshkasera.ppkk@gmail.com>
2025-03-21 15:27:55 +0530

    fixed backend url

--- frontend/src/Utility/constants.js
+++ frontend/src/Utility/constants.js
@@ -1,3 +1,3 @@
 //export const API_URL = "http://localhost:4000/api";
 //export const API_URL = "http://13.127.40.5:4000/api";
-export const API_URL = "https://backendcollection.onrender.com"
+export const API_URL = "https://backendcollection.onrender.com/api"
